af2693b049606b58ca9b837955af818842319fb9
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-09-02 21:05:48 -0300

    initial commit

---
+++ manifests/default.pp
@@ -0,0 +1,21 @@
+include apt
+include apt::update
+
+apt::ppa { 'ppa:webupd8team/java': }
+
+exec { 'setup-java-license-1':
+    command => 'echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections',
+    path => '/bin:/usr/bin'
+}
+exec { 'setup-java-license-2':
+    command => 'echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections',
+    require => Exec['setup-java-license-1'],
+    path => '/bin:/usr/bin'
+}
+package { 'oracle-java7-installer':
+    require => [ Exec['apt_update'],
+            Exec['setup-java-license-2'] ]
+}
+package { 'chromium-browser':
+    require => Exec['apt_update'] }
+
